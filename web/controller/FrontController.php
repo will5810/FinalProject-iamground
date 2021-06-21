9ac1798ccf25fa3df4f95ce429606a51386d3517
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-21 19:21:27 +0900

    210621 update kosta final project
    -front controller

---
+++ web/controller/FrontController.php
@@ -0,0 +1,76 @@
+<?php
+    echo "<script> console.log('frontcontroller load'); </script>";
+    require_once 'UserController.php';
+    require_once 'MoController.php';
+    require_once 'MapController.php';
+    
+    class FrontController{
+        private $controller;
+        private $action;
+        private $data;
+        private $view;
+        private $query;
+        public $responseData = array();
+
+        public function __construct($query=null){
+            
+            echo "<script> console.log('construct frontcontroller'); </script>";
+            $this->query = $query;
+            echo "<script> console.log('".$this->query."'); </script>";
+            
+            //$this->action = $action; 
+                               
+        }
+        
+        public function run(){
+            
+            switch($this->query){
+                case "action=login":
+                    //echo $this->query;
+                    $this->controller = new UserController();
+                    $this->controller->login($_POST['uid'], $_POST['pwd']);
+                    //$this->login();
+                    break;
+                case "action=moList":
+                    //echo $this->query;
+                    $this->controller = new MoController();
+                    $this->controller->cSelectMoByMapId('000001');
+                    break;
+                case "action=mapList":
+                    $this->controller = new MapController();
+                    session_start();
+                    //$this->controller->cSelectMapByUserId($_SESSION['uid']);
+                    $this->controller->cSelectMapByUserId('yun');
+                    break;
+            }
+            
+        }
+        /*
+        public function login(){   
+            $code = $this->userService->login($_POST['uid'], $_POST['pwd']);                        
+            switch($code){
+                case 1:
+                    $this->data = 'no id';
+                    require './view/loginForm.php';                     
+                case 2:
+                    $this->data = 'wrong password';
+                    require './view/loginForm.php';                    
+                case 3:
+                    echo "<script> console.log('login ok'); </script>";
+                    $this->data = 'login ok';
+                    require './view/main.php';                 
+                                     
+            }
+        }
+        */
+        public function controlView($view, $responseData=null){
+            echo "<script> console.log('controlView'); </script>";
+            $this->responseData = $responseData;     
+            //var_dump($this->responseData); 
+            echo '11';  
+            require './view/'.$view;
+        }
+
+
+    }
+?>
