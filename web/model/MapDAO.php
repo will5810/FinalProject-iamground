c8952b2d99754d6272133faae5bf287ae51dd9c4
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-24 00:29:11 +0900

    210623 update kosta final project
    - map dao

--- web/model/MapDAO.php
+++ web/model/MapDAO.php
@@ -32,11 +32,56 @@
             $stmt->execute();
 
             $stmt->bind_result($mid, $uid, $location);
+
+            
             while($stmt->fetch()){
                 $userDTOs[] = new MapDTO($mid, $uid, $location);
             }
 
             return $userDTOs;
         }
+
+
+	public function insertByMap(){		
+		$sql = "insert into map values(?,?)";
+		$stm = $this->conn->prepare ($sql);
+		$stm->bindValue (1, $this->getMapId ());
+		$stm->bindValue (2, $this->getMapLocation ());
+		$stm->execute();
+	}
+
+	public function updateByMap(){
+		$sql = "update map set map_id=?, map_location=? where user_id=?;";		
+		$stm = $this->conn->prepare ($sql);
+		$stm->bindValue (1, $m->getMapId ());
+		$stm->bindValue (2, $m->getMapLocation ());
+		$stm->bindValue (3, $m->getUserId ());
+		$stm->execute();
+	}
+	public function deleteByMap($userId){
+		$sql = "delete from map where user_id=?;";
+		$stm = $this->conn->prepare ($sql);
+		$stm->bindValue (1, $userId);
+		$stm->execute();
+	}
+        
+        public function mMonitoringByMoId($moId){
+            $dtoArr = array();
+            $sql = "SELECT m.map_id, m.user_id, m.map_location, mo.mo_id, mo.mo_type FROM map AS m JOIN mobile_object AS mo ON m.map_Id = mo.map_Id WHERE mo.mo_id = ?";
+            $stmt = $this->connection->prepare($sql);
+
+            $stmt->bind_param('s', $moId);
+            $stmt->execute();
+
+            $stmt->bind_result($mid, $uid, $location, $moid, $motype);
+            if($stmt->fetch()){
+                $mapDTO = new MapDTO($mid, $uid, $location);
+                $moDTO = new MoDTO($moid, null, null, $motype);
+                array_push($dtoArr, $mapDTO);
+                array_push($dtoArr, $moDTO);    
+            }
+            return $dtoArr;
+        }
+        
     }
 ?>
