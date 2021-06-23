5ecaa2eb63aaba0d9c688216a69e0e4f666fbd01
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-24 00:35:19 +0900

    210623 update kosta final project
    - mo dao

--- web/model/MoDAO.php
+++ web/model/MoDAO.php
@@ -26,11 +26,11 @@
             echo "<script> console.log('mo controller mSelectMoByMapId()'); </script>";
             
             $moDTOs = array();
-            $sql = "SELECT * FROM mobile_object WHERE mo_type='1'";
+            $sql = "SELECT * FROM mobile_object WHERE map_id=?";
             //$sql = "SELECT * FROM mobile_object";
             $stmt = $this->connection->prepare($sql);
             
-            //$stmt->bind_param('s', $mapId);           
+            $stmt->bind_param('s', $mapId);           
             $stmt->execute();            
             
 
@@ -42,5 +42,28 @@
             
             return $moDTOs;
         }
+	public function insertByMo(){		
+		$sql = "insert into mobile_object values(?,?)";
+		$stm = $this->conn->prepare ($sql);
+		$stm->bindValue (1, $this->getMoId ());
+		$stm->bindValue (2, $this->getMoType ());
+		$stm->execute();										
+	}
+
+	public function updateByMo(){
+		$sql = "update mobile_object set mo_id=?, mo_type=? where user_id=?;";		
+		$stm = $this->conn->prepare ($sql);
+		$stm->bindValue (1, $m->getMoId ());
+		$stm->bindValue (2, $m->getMoType ());
+		$stm->bindValue (3, $m->getUserId ());
+		$stm->execute();									
+	}
+	public function deleteByMo($userId){
+		$sql = "delete from mobile_object where user_id=?;";
+		$stm = $this->conn->prepare ($sql);
+		$stm->bindValue (1, $userId);
+		$stm->execute();
+	}
+
     }
 ?>
