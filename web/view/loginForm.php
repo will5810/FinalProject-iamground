076793d2abffef16c18911916dfde4cb48610b64
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-24 00:43:05 +0900

    210623 update kosta final project
    - loginForm

--- web/view/loginForm.php
+++ web/view/loginForm.php
@@ -7,23 +7,24 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>php로그인</title>      
+    <title>iamgorund</title>
+    <link rel="stylesheet" href="./source/iamground.css">     
   </head>
   <body>
-    <form  method="post" action="../index.php?action=login">
-    로그인</br>
-    <table>
-      <tr>
-        <td>아이디 :</td>
-        <td><input type="text" name="uid"/></td>
-        <td rowspan=2><input type="submit" value="sign in" /></td>
-      </tr>
-      <tr>
-        <td>비밀번호 : </td>
-        <td><input type="password" name="pwd" /></td>
-      </tr>
-    </table>
-      
-    </form>
+  <div class='container'>
+    <form method="post" action="./index.php?action=login"> 
+      <table id="loginTable">
+        <tr>
+          <td>아이디 :</td>
+          <td><input type="text" name="uid"/></td>
+          <td rowspan=2><input class="btnLogin" type="submit" value="sign in" /></td>
+        </tr>
+        <tr>
+          <td>비밀번호 : </td>
+          <td><input type="password" name="pwd" /></td>
+        </tr>
+      </table>      
+      </form>    
+  </div>
   </body>
 </html>
