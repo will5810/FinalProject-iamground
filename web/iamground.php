97d8694e6f4c56748235a9a67e80d5fe72b21cd3
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-24 00:40:22 +0900

    210623 update kosta final project
    - iamground.php

---
+++ web/iamground.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <title>Iamground</title>
+    <link rel="stylesheet" href="./source/iamground.css">
+</head>
+<body>
+    <div class='container'>
+        <h1 class='logo'><span class='logoI'>I</span><span class='logoAM'>AM</span><span class='logoGround'>Ground</span></h1>
+        <button onclick="window.location.href='./index.php?action=loginForm'">START</button>        
+    </div>
+</body>
+</html>
